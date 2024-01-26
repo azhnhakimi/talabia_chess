a6c442a597f819a545a166c8a5794ed127d0915c
SyedThahir <syedthahiralhabshi@gmail.com>
2024-01-26 01:46:02 -0800

    Piece can move but not captured

--- model/pieces/Point.java
+++ model/pieces/Point.java
@@ -1,18 +1,18 @@
 package model.pieces;
 
 import java.util.ArrayList;
-
 import model.Position;
 
 public class Point extends Piece {
 
     String pieceType = "point";
     
-    public Point(String colour){
+    public Point(String colour) {
         super(colour);
         super.setPieceType(this.pieceType);
     }
 
+    @Override
     public ArrayList<Position> getPossibleMoves() {
         Position position = this.getCurrentPosition();
         if (position == null) {
@@ -23,22 +23,22 @@ public class Point extends Piece {
         int column = position.getColumn();
         ArrayList<Position> possibleMoves = new ArrayList<>();
 
-        // Check if the piece is at the top or bottom row to reverse direction
+        // Determine the direction based on the color of the piece
         int direction = (this.getColour().equals("white")) ? -1 : 1;
 
         // Point can move 1 step forward
-        if (isWithinBounds(row + direction, column)) {
-            possibleMoves.add(new Position(row + direction, column));
-        }
+        addMoveIfValid(possibleMoves, row + direction, column);
 
-        // Point can move 2 steps forward if it's in its starting position
-        if ((this.getColour().equals("white") && row == 4) || (this.getColour().equals("black") && row == 1)) {
-            if (isWithinBounds(row + 2 * direction, column)) {
-                possibleMoves.add(new Position(row + 2 * direction, column));
-            }
-        }
+        // Point can move 2 steps forward 
+        addMoveIfValid(possibleMoves, row + 2 * direction, column);
 
         return possibleMoves;
+    }
+
+    private void addMoveIfValid(ArrayList<Position> moves, int row, int column) {
+        if (isWithinBounds(row, column)) {
+            moves.add(new Position(row, column));
+        }
     }
 
     private boolean isWithinBounds(int row, int column) {
